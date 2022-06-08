5d770c5906bf17b0f2126fe176b8c2506c229457
starluu <36126288+starluu@users.noreply.github.com>
2022-06-08 21:17:02 +0800

    Update diy-part1.sh

--- Lean/diy-part1.sh
+++ Lean/diy-part1.sh
@@ -29,4 +29,4 @@ sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
 sed -i '$a src-git guyezi https://github.com/guyezi/hello-luci.git' feeds.conf.default
 
 # 添加第三方插件源
-sed -i '$a src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages.git' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
