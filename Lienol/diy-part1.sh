8b62cae9895c38c425189214d7d7bc7e53907654
starluu <36126288+starluu@users.noreply.github.com>
2022-06-08 11:03:11 +0800

    Add files via upload

---
+++ Lienol/diy-part1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 删除原版target.mk
+rm -rf include/target.mk
+
+# 下载新的target.mk
+wget -P /include https://github.com/x-wrt/x-wrt/raw/master/include/target.mk
+
+# 删除原版Makefile
+rm -rf target/linux/x86/Makefile
+
+# 下载新的Makefile
+wget -P /target/linux/x86 https://github.com/x-wrt/x-wrt/raw/master/target/linux/x86/Makefile
+
+# 注释掉默认luci源
+sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+
+# 添加第三方luci源
+sed -i '$a src-git guyezi https://github.com/guyezi/hello-luci.git' feeds.conf.default
+
+# 添加第三方插件源
+sed -i '$a src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages.git' feeds.conf.default
---
+++ Lienol/diy-part2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# 修改target.mk
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+sed -i 's/kmod-nft-offload/luci-app-turboacc/g' include/target.mk
+
+# 修改Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+
+# 取消Lean大登陆密码
+sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
