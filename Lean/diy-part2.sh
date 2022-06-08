53f3ab140ef3b45447aebaedd35c6d4b3ec2baf4
starluu <36126288+starluu@users.noreply.github.com>
2022-06-08 10:15:49 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -16,22 +16,3 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # 取消Lean大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
-
-# 拉取passwall源码
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/packages
-#git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
-
-# 拉取luci-app-passwall插件
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
-#svn co https://github.com/lxhao61/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci
-#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci
-
-# 拉取luci-app-smartdns插件
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
-
-# 删除原版luci-app-softethervpn插件
-rm -rf feeds/luci/applications/luci-app-softethervpn
-
-# 拉取luci-app-softethervpn插件
-svn co https://github.com/lxhao61/openwrt-plugin/trunk/package/lean/luci-app-softethervpn feeds/luci/applications/luci-app-softethervpn
