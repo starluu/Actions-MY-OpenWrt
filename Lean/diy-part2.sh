21cf7d230441a81677ecec3dcce7cc2e4c7faaa5
starluu <36126288+starluu@users.noreply.github.com>
2022-11-09 20:13:43 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.1.6/g' package/base-files/files/bin/config_generate
 
-# 修改Makefile
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.0/g' target/linux/x86/Makefile
+# 删除原版golang
+rm -rf feeds/packages/lang/golang
