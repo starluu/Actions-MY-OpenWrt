5e2f920279efcc22da4191d31f87d7c2b6fc4347
starluu <36126288+starluu@users.noreply.github.com>
2022-11-09 19:43:18 +0800

    Update target.mk

--- Lean/target.mk
+++ Lean/target.mk
@@ -43,10 +43,7 @@ DEFAULT_PACKAGES.nas:=\
 DEFAULT_PACKAGES.router:=\
 	dnsmasq-full firewall iptables ppp ppp-mod-pppoe \
 	block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
-	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi \
-	ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-autoreboot \
-	luci-app-arpbind luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-vlmcsd \
-	luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol curl ca-certificates
+	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi luci-compat \
 
 ifneq ($(DUMP),)
   all: dumpinfo
