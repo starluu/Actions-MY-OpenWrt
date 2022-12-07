61a366beb6dbc4b99490e48bd3bdf409dfa35dc4
starluu <36126288+starluu@users.noreply.github.com>
2022-12-07 09:57:45 +0800

    Update diy-part1.sh

--- Lienol/diy-part1.sh
+++ Lienol/diy-part1.sh
@@ -10,17 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 删除原版target.mk
-rm -rf include/target.mk
-
-# 下载新的target.mk
-wget -P include https://github.com/x-wrt/x-wrt/raw/master/include/target.mk
-
-# 删除原版Makefile
-rm -rf target/linux/x86/Makefile
-
-# 下载新的Makefile
-wget -P target/linux/x86 https://github.com/x-wrt/x-wrt/raw/master/target/linux/x86/Makefile
-
 # 添加第三方插件源
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
