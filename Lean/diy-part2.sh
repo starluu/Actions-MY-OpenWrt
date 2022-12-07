009251974713cf930f49e5343be8c23e3393e734
starluu <36126288+starluu@users.noreply.github.com>
2022-12-07 12:28:26 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -9,3 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# 删除原版golang添加Alist支持
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+
+# replace default passwall2
+rm -rf feeds/luci/applications/luci-app-passwall2
+svn export https://github.com/smallprogram/openwrt-passwall2/trunk feeds/luci/applications/luci-app-passwall2
