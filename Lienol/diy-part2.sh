f60483d982e3b549d334185bc940ae5b21917ea6
starluu <36126288+starluu@users.noreply.github.com>
2022-12-07 10:01:03 +0800

    Update diy-part2.sh

--- Lienol/diy-part2.sh
+++ Lienol/diy-part2.sh
@@ -13,6 +13,9 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
+# 修改Kernel版本
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.0/g' target/linux/x86/Makefile
+
 # 修改target.mk
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 
