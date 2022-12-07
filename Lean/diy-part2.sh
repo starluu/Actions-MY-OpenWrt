e65f4b450424392403d18ea6653274c6f698117a
starluu <36126288+starluu@users.noreply.github.com>
2022-12-07 15:54:48 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -12,7 +12,3 @@
 # 删除原版golang添加Alist支持
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
-
-# replace default passwall2
-rm -rf feeds/luci/applications/luci-app-passwall2
-svn export https://github.com/smallprogram/openwrt-passwall2/trunk feeds/luci/applications/luci-app-passwall2
