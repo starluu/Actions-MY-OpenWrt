4b3506b90716ceb88de6edbb365810e11370e3cb
starluu <36126288+starluu@users.noreply.github.com>
2022-12-09 09:03:00 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -10,11 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
-# 添加第三方插件
-svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-vsftpd feeds/luci/applications/luci-app-vsftpd
-svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-diskman feeds/luci/applications/luci-app-diskman
-
 # 删除原版golang添加Alist支持
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
