a64691b99773d8b8be056bee6f1af3bad3eecce5
starluu <36126288+starluu@users.noreply.github.com>
2022-12-13 15:45:50 +0800

    Update diy-part2.sh

--- Lienol/diy-part2.sh
+++ Lienol/diy-part2.sh
@@ -13,9 +13,6 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 修改target.mk
-sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
-
 # 删除原版golang添加Alist支持
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
