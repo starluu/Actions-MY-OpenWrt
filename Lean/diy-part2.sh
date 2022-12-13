68409a498a11dadcf56c71c388a6d5120965aad1
starluu <36126288+starluu@users.noreply.github.com>
2022-12-13 17:11:21 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -10,6 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 删除原版golang添加Alist支持
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
