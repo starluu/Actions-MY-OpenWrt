0f1fc8e7575e1a0ad4ca0dc121104acc7f883e36
starluu <36126288+starluu@users.noreply.github.com>
2022-12-13 14:38:46 +0800

    Update target.mk

--- Lean/target.mk
+++ Lean/target.mk
@@ -42,9 +42,10 @@ DEFAULT_PACKAGES.nas:=\
 # For router targets
 DEFAULT_PACKAGES.router:=\
 	dnsmasq-full \
-	firewall4 \
-	nftables \
-	kmod-nft-offload \
+	firewall \
+	ip6tables \
+	iptables \
+	kmod-ipt-offload \
 	odhcp6c \
 	odhcpd-ipv6only \
 	ppp \
