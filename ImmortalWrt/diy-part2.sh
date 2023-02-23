c3c3f6801edfa433e3b0a079f7db8d62b4246097
starluu <36126288+starluu@users.noreply.github.com>
2023-02-24 07:38:10 +0800

    Update diy-part2.sh

--- ImmortalWrt/diy-part2.sh
+++ ImmortalWrt/diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-# Modify default HostName
-sed -i 's/ImmortalWrt//g' package/base-files/files/bin/config_generate
