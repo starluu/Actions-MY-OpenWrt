673d942ffd59c84ffe62ac308bcc6c22a1ab0881
starluu <36126288+starluu@users.noreply.github.com>
2023-02-23 10:31:30 +0800

    Update diy-part2.sh

--- ImmortalWrt/diy-part2.sh
+++ ImmortalWrt/diy-part2.sh
@@ -14,8 +14,4 @@
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default HostName
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-
-# 删除原版golang添加Alist支持
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+sed -i 's/ImmortalWrt//g' package/base-files/files/bin/config_generate
