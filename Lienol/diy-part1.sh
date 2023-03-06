fe75a982494936447a5c01f5e19ccd9a27b281a1
starluu <36126288+starluu@users.noreply.github.com>
2023-03-06 18:43:19 +0800

    Update diy-part1.sh

--- Lienol/diy-part1.sh
+++ Lienol/diy-part1.sh
@@ -10,12 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 修改原版luci版本
-sed -i 's/17.01-dev/master/g' feeds.conf.default
-
 # 修改原版默认插件
 sed -i 's/luci-app-timecontrol/luci-app-alist/g' include/target.mk
-sed -i 's/luci-app-control-weburl/kmod-nft-offload/g' include/target.mk
+sed -i 's/luci-app-control-weburl/luci-app-turboacc/g' include/target.mk
 sed -i 's/luci-app-control-timewol/luci-app-diskman/g' include/target.mk
 sed -i 's/luci-app-control-webrestriction/luci-app-aliyundrive-fuse/g' include/target.mk
 sed -i 's/autosamba/luci-app-samba4/g' target/linux/x86/Makefile
