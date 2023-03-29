6706cf2c4c6b67419e1b71ae19c86f7a0ac02345
starluu <36126288+starluu@users.noreply.github.com>
2023-03-29 15:54:53 +0800

    Update diy-part2.sh

--- ImmortalWrt/diy-part2.sh
+++ ImmortalWrt/diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-# Modify default kernel
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
