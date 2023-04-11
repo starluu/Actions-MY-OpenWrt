3b576143cf03770c73272d8898ab3fd9c08332ba
starluu <36126288+starluu@users.noreply.github.com>
2023-04-11 08:52:45 +0800

    Add files via upload

---
+++ ImmortalWrt/diy-part2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# Modify default HostName
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# 取消Immortal大登陆密码
+sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
+
+# 删除原版golang添加Alist支持
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
