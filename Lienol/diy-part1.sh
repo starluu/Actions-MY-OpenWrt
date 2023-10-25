a06f8072f2fc5754c8ca20b6bf400231c6dd4eb3
starluu <36126288+starluu@users.noreply.github.com>
2023-10-26 06:09:44 +0800

    Update diy-part1.sh

--- Lienol/diy-part1.sh
+++ Lienol/diy-part1.sh
@@ -9,21 +9,5 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# 删除原版target.mk
-rm -rf include/target.mk
-
-# 下载新的target.mk
-wget -P include https://github.com/starluu/Actions-MY-OpenWrt/raw/main/Lienol/target.mk
-
-# 修改原版默认插件
-sed -i 's/autosamba/luci-app-samba4/g' target/linux/x86/Makefile
-sed -i 's/luci-app-usb-printer/openssh-sftp-server/g' target/linux/x86/Makefile
-
-# 取消原版lienol
-sed -i 's/^\(.*lienol\)/#&/' feeds.conf.default
-
-# 取消原版other
-sed -i 's/^\(.*other\)/#&/' feeds.conf.default
-
 # 添加第三方插件源
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
