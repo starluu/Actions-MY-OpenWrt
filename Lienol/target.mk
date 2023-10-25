4e8ed3ffd396ad5f8387ee4f170c694edf42ab55
starluu <36126288+starluu@users.noreply.github.com>
2023-10-26 00:22:26 +0800

    Update target.mk

--- Lienol/target.mk
+++ Lienol/target.mk
@@ -25,11 +25,7 @@ DEFAULT_PACKAGES:=\
 	uci \
 	uclient-fetch \
 	urandom-seed \
-	luci luci-compat wget-ssl curl ca-certificates htop \
-	default-settings luci-app-upnp luci-app-wol luci-app-vlmcsd luci-app-ramfree \
-	luci-app-ddns ddns-scripts-cloudflare ddns-scripts_aliyun ddns-scripts_dnspod \
-	luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl \
-	urngd
+	luci luci-compat
 
 ifneq ($(CONFIG_SELINUX),)
 DEFAULT_PACKAGES+=busybox-selinux procd-selinux
