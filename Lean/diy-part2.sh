a96e6db9c634f31c8994e17eacef8f371ce7af2d
starluu <36126288+starluu@users.noreply.github.com>
2023-10-30 05:12:38 +0800

    Update diy-part2.sh

--- Lean/diy-part2.sh
+++ Lean/diy-part2.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 删除旧版applications
-rm -rf feeds/luci/applications
+# 删除旧版themes
+rm -rf feeds/luci/themes
