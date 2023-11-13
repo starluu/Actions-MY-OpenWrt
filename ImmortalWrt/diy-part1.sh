f47f10613c7221c50601c308d78e589c21e63965
starluu <36126288+starluu@users.noreply.github.com>
2023-11-13 09:30:54 +0800

    Update diy-part1.sh

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -9,8 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# 添加第三方插件源
-
-# 修改内核版本
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
