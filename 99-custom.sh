86c7cabaef224b4506ffc04ce9b06ae4cb385716
starluu <36126288+starluu@users.noreply.github.com>
2025-05-26 09:06:59 +0800

    Add files via upload

---
+++ 99-custom.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# Log file for debugging
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
+
+# 多网口设备 支持修改为别的ip地址
+uci set network.lan.ipaddr='192.168.1.1'
+uci commit
+
+# 删除编译作者信息
+rm -rf /usr/lib/lua/luci/view/admin_status/index/links.htm
+
+exit 0
