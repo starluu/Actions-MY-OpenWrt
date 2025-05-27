67e1f174bd80b5a5186619b71e28da7110c71cb3
starluu <36126288+starluu@users.noreply.github.com>
2025-05-27 18:31:29 +0800

    Update 99-custom.sh

--- kwrt/99-custom.sh
+++ kwrt/99-custom.sh
@@ -6,6 +6,7 @@ echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 
 # 多网口设备 支持修改为别的ip地址
 uci set network.lan.ipaddr='192.168.1.1'
+uci set network.lan.gateway='192.168.1.2'
 uci add_list network.lan.dns='1.1.1.1'
 uci add_list network.lan.dns='8.8.8.8'
 uci add_list network.lan.dns='192.168.1.2'
@@ -18,6 +19,6 @@ rm -rf /usr/lib/lua/luci/view/admin_status/index/links.htm
 opkg --force-removal-of-dependent-packages --autoremove remove luci-app-partexp
 
 # 安装最新alist
-opkg install /etc/alist/*.ipk  --force-depends
+opkg install /etc/alist/packages_ci/*.ipk  --force-depends
 
 exit 0
