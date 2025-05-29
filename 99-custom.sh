342992f6a06b7549e58e5d8702beb123bf43d2ba
starluu <36126288+starluu@users.noreply.github.com>
2025-05-29 12:39:23 +0800

    Add files via upload

---
+++ 99-custom.sh
@@ -0,0 +1,30 @@
+etc#!/bin/sh
+# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# Log file for debugging
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
+
+# 多网口设备 支持修改为别的ip地址
+uci set network.lan.ipaddr='192.168.1.1'
+uci set network.lan.gateway='192.168.1.2'
+uci add_list network.lan.dns='1.1.1.1'
+uci add_list network.lan.dns='8.8.8.8'
+uci add_list network.lan.dns='192.168.1.2'
+uci commit
+
+# 删除作者广告信息
+rm -rf /usr/lib/lua/luci/view/admin_status/index/links.htm
+
+# 卸载顽固预装软件
+opkg --force-removal-of-dependent-packages --autoremove remove luci-app-partexp
+
+# 修改国内软件源
+sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' /etc/opkg/distfeeds.conf
+sed -i 's/openwrt/immortalwrt/g' /etc/opkg/distfeeds.conf
+
+# 安装最新alist及其他app
+opkg update
+opkg install /etc/alist/packages_ci/*.ipk  --force-depends
+rm -rf /etc/alist/*
+
+exit 0
