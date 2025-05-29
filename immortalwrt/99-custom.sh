9778e94fc25c577e628f528debacbee2f4a3cd3e
starluu <36126288+starluu@users.noreply.github.com>
2025-05-29 12:41:01 +0800

    Update and rename 99-custom.sh to immortalwrt/99-custom.sh

--- immortalwrt/99-custom.sh
+++ immortalwrt/99-custom.sh
@@ -12,12 +12,6 @@ uci add_list network.lan.dns='8.8.8.8'
 uci add_list network.lan.dns='192.168.1.2'
 uci commit
 
-# 删除作者广告信息
-rm -rf /usr/lib/lua/luci/view/admin_status/index/links.htm
-
-# 卸载顽固预装软件
-opkg --force-removal-of-dependent-packages --autoremove remove luci-app-partexp
-
 # 修改国内软件源
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' /etc/opkg/distfeeds.conf
 sed -i 's/openwrt/immortalwrt/g' /etc/opkg/distfeeds.conf
