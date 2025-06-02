afb7db0388cec6a072bc1d01205d0231e650a7ee
starluu <36126288+starluu@users.noreply.github.com>
2025-06-02 12:33:46 +0800

    Delete qbittorrent-nox-static-install.sh

--- qbittorrent-nox-static-install.sh
+++
@@ -1,2 +0,0 @@
-qbittorrent-nox_repo=$(curl -s https://api.github.com/repos/userdocs/qbittorrent-nox-static/releases/latest | grep "browser_download_url.*x86_64-qbittorrent-nox" | cut -d '"' -f 4)
-bash <(wget ${qbittorrent-nox_repo})
