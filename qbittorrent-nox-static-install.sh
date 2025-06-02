4b2bdd2ae4533b2b75cdfa6a2f10731cfed27feb
starluu <36126288+starluu@users.noreply.github.com>
2025-06-02 12:30:18 +0800

    Update qbittorrent-nox-static-install.sh

--- qbittorrent-nox-static-install.sh
+++ qbittorrent-nox-static-install.sh
@@ -1,2 +1,2 @@
 qbittorrent-nox_repo=$(curl -s https://api.github.com/repos/userdocs/qbittorrent-nox-static/releases/latest | grep "browser_download_url.*x86_64-qbittorrent-nox" | cut -d '"' -f 4)
-wget ${qbittorrent-nox_repo}
+bash <(wget ${qbittorrent-nox_repo})
