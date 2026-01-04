b6aaa1b374f770c783c260fff92ccb077d928709
starluu <36126288+starluu@users.noreply.github.com>
2026-01-04 19:51:52 +0800

    Update 99-custom.sh

--- immortalwrt/99-custom.sh
+++ immortalwrt/99-custom.sh
@@ -18,7 +18,4 @@ uci commit
 # 修改源地址
 sed -i 's|downloads.immortalwrt.org|mirrors.shanghaitech.edu.cn/immortalwrt|g' /etc/apk/repositories.d/distfeeds.list
 
-# 卸载顽固预装软件
-apk del luci-app-attendedsysupgrade
-
 exit 0
