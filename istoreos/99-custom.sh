bf832b0a2224d5757ee462ac57473e698ac0d59f
starluu <36126288+starluu@users.noreply.github.com>
2026-05-10 17:16:13 +0800

    Update 99-custom.sh

--- istoreos/99-custom.sh
+++ istoreos/99-custom.sh
@@ -11,6 +11,8 @@ uci set network.lan.gateway='192.168.1.2'
 uci add_list network.lan.dns='1.1.1.1'
 uci add_list network.lan.dns='8.8.8.8'
 uci add_list network.lan.dns='192.168.1.2'
+uci set system.@system[0].timezone='CST-8'
+uci set system.@system[0].zonename='Asia/Shanghai'
 uci commit
 
 exit 0
