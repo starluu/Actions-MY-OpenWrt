c35562682353f19c481a3c467ffa435691f2ab8c
starluu <36126288+starluu@users.noreply.github.com>
2026-06-20 14:18:46 +0800

    Create 99-custom.sh

---
+++ immortalwrt/99-custom.sh
@@ -0,0 +1,16 @@
+etc#!/bin/sh
+# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# Log file for debugging
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
+
+# 多网口设备 支持修改为别的ip地址
+uci set network.lan.gateway='192.168.1.2'
+uci add_list network.lan.dns='1.1.1.1'
+uci add_list network.lan.dns='8.8.8.8'
+uci add_list network.lan.dns='192.168.1.2'
+uci set system.@system[0].timezone='CST-8'
+uci set system.@system[0].zonename='Asia/Shanghai'
+uci commit
+
+exit 0
